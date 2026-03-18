47ff21253fc41831ad0786d54b714c0cda159a8f
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2026-03-18 16:54:49 -0500

    fix:dashboard

--- src/components/dashboard/HomeView.tsx
+++ src/components/dashboard/HomeView.tsx
@@ -8,10 +8,8 @@ interface HomeViewProps {
 
 export const HomeView: React.FC<HomeViewProps> = ({ user }) => {
     return (
-        <main className="lg:ml-32 ml-0 pt-20 py-6 px-4 sm:px-6 lg:px-8">
-            <div className="px-4 py-6 sm:px-0">
-                <WelcomeCard userName={user?.name} />
-            </div>
-        </main>
+        <div className="py-4 px-4 sm:px-0">
+            <WelcomeCard userName={user?.name} />
+        </div>
     );
 };
