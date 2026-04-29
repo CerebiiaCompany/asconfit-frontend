1fff5d0575f9ba50d0c552cd1a71b0ebd066130f
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2026-04-28 23:06:55 -0500

    fix: storageUrl for images, fix pdf.js worker source

--- src/components/companies/WorkPapers.tsx
+++ src/components/companies/WorkPapers.tsx
@@ -7,7 +7,10 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 
 // Carga el PDF como blob para evitar problemas de CORS con el worker de pdf.js
 function usePdfBlob(url: string) {
