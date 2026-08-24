4a031272445995ea638a28e7ef39d725f885c15f
Jhon monsalve <99751739+Assasin1116@users.noreply.github.com>
2026-08-24 15:07:04 -0500

    fix: change my tasks

--- src/pages/MisTareas.tsx
+++ src/pages/MisTareas.tsx
@@ -104,7 +104,7 @@ export const MisTareas: React.FC = () => {
             )}
             <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">
               {auditoriaSeleccionada === null
-                ? "Mis Encargos"
+                ? "Mis Tareas"
                 : "Encargos de la Auditoría"}
             </h1>
           </div>
